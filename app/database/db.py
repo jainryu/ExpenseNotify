4e23ba42da80ca9b38a25c98576959200a95b34d
jainryu <79353841+jainryu@users.noreply.github.com>
2025-04-29 23:10:30 -0700

    db change

--- app/database/db.py
+++ app/database/db.py
@@ -10,3 +10,8 @@ async def get_transactions() -> list[Transaction]:
     items = response.get('Items', [])
     
     return [Transaction.model_validate(item) for item in items]
+
+async def create_transaction(transaction: Transaction) -> Transaction:
+    table.create_item(
+        Item=transaction.model_dump()
+    )
