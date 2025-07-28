6b33974587e0cede17fda238a15537c11d03389d
jainryu <79353841+jainryu@users.noreply.github.com>
2025-07-27 23:31:30 -0700

    add middleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import Depends, FastAPI
 from app.api.dependencies import get_db, get_gemini_client
 from app.models.Transaction import Transaction
 from app.routers import auth, genai, home, transaction
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.service.transaction_db import DB
 from app.service.gemini import Gemini
@@ -13,3 +15,11 @@ app.include_router(transaction.router)
 app.include_router(home.router)
 app.include_router(genai.router)
 app.include_router(auth.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # your frontend's origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
